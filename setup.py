73ac15e8c076d4ea446965664118b639a7aceb3c
Karel Kočí <karel.koci@nic.cz>
2019-11-12 13:38:28 +0100

    Update URLs to Foris repositories

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     description=DESCRIPTION,
     long_description=open("README.rst").read(),
     install_requires=[
-        "foris-controller @ git+https://gitlab.labs.nic.cz/turris/foris-controller.git"
+        "foris-controller @ git+https://gitlab.labs.nic.cz/turris/foris-controller/foris-controller.git"
     ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest", "foris-controller-testtools", "foris-client", "ubus", "paho-mqtt"],
@@ -53,8 +53,8 @@ setup(
         ]
     },
     dependency_links=[
-        "git+https://gitlab.labs.nic.cz/turris/foris-controller-testtools.git#egg=foris-controller-testtools",
-        "git+https://gitlab.labs.nic.cz/turris/foris-client.git#egg=foris-client",
+        "git+https://gitlab.labs.nic.cz/turris/foris-controller/foris-controller-testtools.git#egg=foris-controller-testtools",
+        "git+https://gitlab.labs.nic.cz/turris/foris-controller/foris-client.git#egg=foris-client",
     ],
     include_package_data=True,
     zip_safe=False,
