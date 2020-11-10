3d3c33f723ab0c8b3c70036281037a7be0444a76
Martin Matějek <martin.matejek@nic.cz>
2020-11-10 19:19:28 +0100

    CI: add flake8 to CI pipeline

--- setup.py
+++ setup.py
@@ -45,7 +45,10 @@ setup(
     install_requires=[
         "foris-controller @ git+https://gitlab.nic.cz/turris/foris-controller/foris-controller.git"
     ],
-    setup_requires=["pytest-runner"],
+    setup_requires=[
+        "pytest-runner",
+        "flake8",
+    ],
     tests_require=["pytest", "foris-controller-testtools", "foris-client", "ubus", "paho-mqtt"],
     dependency_links=[
         "git+https://gitlab.nic.cz/turris/foris-controller/foris-controller-testtools.git#egg=foris-controller-testtools",
