33004c85955d7142bd390f31d8db50d287bddfdc
Stepan Henek <stepan.henek@nic.cz>
2023-08-22 11:01:35 +0200

    CI: start using tox and publish python packages into registry

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #
 # foris-controller-openvpn_client-module
-# Copyright (C) 2019-2020, 2022 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
+# Copyright (C) 2023 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -42,18 +42,9 @@ setup(
     namespace_packages=["foris_controller_modules", "foris_controller_backends"],
     description=DESCRIPTION,
     long_description=open("README.rst").read(),
-    install_requires=[
-        "foris-controller @ git+https://gitlab.nic.cz/turris/foris-controller/foris-controller.git"
-    ],
-    setup_requires=[
-        "pytest-runner",
-        "flake8<4.0",  # setuptools integration with flake8 is deprecated in version >=4.0
-    ],
+    install_requires=["foris-controller"],
+    setup_requires=["pytest-runner"],
     tests_require=["pytest", "foris-controller-testtools", "foris-client", "ubus", "paho-mqtt"],
-    dependency_links=[
-        "git+https://gitlab.nic.cz/turris/foris-controller/foris-controller-testtools.git#egg=foris-controller-testtools",
-        "git+https://gitlab.nic.cz/turris/foris-controller/foris-client.git#egg=foris-client",
-    ],
     include_package_data=True,
     zip_safe=False,
 )
